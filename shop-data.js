fa58f3a39d99ea5993d062d6bfc8bd9a3958dce1
Info-Unterricht <yannik.buedenbender@gmx.de>
2026-04-13 08:28:04 +0200

    Update shop-data.js

--- shop-data.js
+++ shop-data.js
@@ -1,18 +1,18 @@
 const SHOP_ITEMS = {
     skins: [
         { id: 'default', name: 'Standard Robi', price: 0, img: 'robi_right.png' },
-        { id: 'skin_gold', name: 'Gold-Robi', price: 1, icon: '🤖', img: 'robi_gold.png' },
-        { id: 'skin_pirate', name: 'Piraten-Robi', price: 1, img: 'robi_pirate.png' },
-        { id: 'skin_ninja', name: 'Ninja-Robi', price: 1, img: 'robi_ninja.png' },
-        { id: 'skin_disco', name: 'Disco-Robi', price: 1, img: 'robi_disco.svg' },
+        { id: 'skin_gold', name: 'Gold-Robi', price: 15, icon: '🤖', img: 'robi_gold.png' },
+        { id: 'skin_pirate', name: 'Piraten-Robi', price: 25, img: 'robi_pirate.png' },
+        { id: 'skin_ninja', name: 'Ninja-Robi', price: 50, img: 'robi_ninja.png' },
+        { id: 'skin_disco', name: 'Disco-Robi', price: 75, img: 'robi_disco.svg' },
 
     ],
     spuren: [
-        { id: 'trail_rainbow', name: 'Regenbogen', price: 1, icon: '🌈' },
-        { id: 'trail_bubbles', name: 'Seifenblasen', price: 1, img: 'soap_bubble.svg' },
-        { id: 'trail_stardust', name: 'Sternenstaub', price: 1, icon: '✨' },
-        { id: 'trail_matrix', name: 'Matrix-Code', price: 1, icon: '📟' },
-        { id: 'trail_steps', name: 'Fußspuren', price: 1, icon: '👣' }
+        { id: 'trail_rainbow', name: 'Regenbogen', price: 10, icon: '🌈' },
+        { id: 'trail_bubbles', name: 'Seifenblasen', price: 30, img: 'soap_bubble.svg' },
+        { id: 'trail_stardust', name: 'Sternenstaub', price: 40, icon: '✨' },
+        { id: 'trail_matrix', name: 'Matrix-Code', price: 50, icon: '📟' },
+        { id: 'trail_steps', name: 'Fußspuren', price: 60, icon: '👣' }
     ],
     welten: [
         { id: 'world_space', name: 'Weltraum', price: 100, icon: '🚀' }
